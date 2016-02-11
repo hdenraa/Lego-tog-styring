a854529a4ef03c0ae46cb1f4a708c5e5399510a0
Kristian Nygaard Jensen <freeduck@member.fsf.org>
2016-02-11 23:43:08 +0100

    Call app

--- html/js/control.js
+++ html/js/control.js
@@ -1,5 +1,15 @@
 jQuery(document).bind("move-ahead", function (){
-    alert('Hjort');
+    jQuery.get("http://localhost:5000/start", function(){
+        alert('success');
+    })
+        .done(function() {
+            alert( "second success" );
+        })
+        .fail(function(e){
+            console.log(e);
+            alert('Fail');
+        });
+
 })
 
 jQuery(document).bind("stop-engine", function (){
