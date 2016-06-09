959445ba18f47a23cbffa6bf630cff06738b9091
Lars Hostrup <larshope@gmail.com>
2016-06-09 23:28:00 +0200

    add gauch

--- html/js/control.js
+++ html/js/control.js
@@ -1,8 +1,30 @@
-jQuery(document).bind("move-ahead", function (){
+google.charts.load('current', {'packages':['gauge']});
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+
+    var data = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Speed', 0],
+       ]);
+
+    var options = {
+        width: 400, height: 120,
+        redFrom: 90, redTo: 100,
+        yellowFrom:75, yellowTo: 90,
+        minorTicks: 5
+    };
+
+    var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+}
+
+    jQuery(document).bind("move-ahead", function (){
     jQuery.get("/app/delta/25")
-        .done(function(speed) {
-            console.log(speed)
-        })
+            .done(function(speed) {
+                data.setValue(0, 1,speed.speed);
+                chart.draw(data, options);
+          })
         .fail(function(e){
             console.log(e);
         });
@@ -11,6 +33,8 @@ jQuery(document).bind("move-ahead", function (){
 jQuery(document).bind("decrease-speed",function(){
     jQuery.get("/app/delta/-25")
         .done(function(speed){
+            data.setValue(0, 1,speed.speed);
+            chart.draw(data, options);
         })
         .fail(function(e){
             console.log(e);
